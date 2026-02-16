ab9b514bf010a2748b55c47d814b63fa441b54cc
Har Aziz Singh <76054014+SinghAziz@users.noreply.github.com>
2026-02-16 11:56:58 +0530

    Create 1024-TriplesWithBitwiseAndEqualToZero

---
+++ 1024-TriplesWithBitwiseAndEqualToZero/1024-TriplesWithBitwiseAndEqualToZero.cpp
@@ -0,0 +1,20 @@
+// Last updated: 16/02/2026, 11:56:58
+class Solution {
+public:
+    int countTriplets(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(auto i: nums){
+            for(auto j: nums){
+                mp[i&j]++;
+            }
+        }
+        int ans = 0;
+        for(auto i: nums){
+            for(auto j: mp){
+                if((i&j.first)==0)
+                ans += j.second;
+            }
+        }
+        return ans;
+    }
+};
