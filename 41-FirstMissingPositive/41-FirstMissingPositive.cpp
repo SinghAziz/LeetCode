81f7ca2d8f9700f3bcbc0288d7c690b65d0309a0
Har Aziz Singh <76054014+SinghAziz@users.noreply.github.com>
2026-03-19 12:55:59 +0530

    File updated at 19/03/2026, 12:55:59

--- 41-FirstMissingPositive/41-FirstMissingPositive.cpp
+++ 41-FirstMissingPositive/41-FirstMissingPositive.cpp
@@ -1,16 +1,20 @@
-// Last updated: 18/03/2026, 01:25:12
+// Last updated: 19/03/2026, 12:55:59
 1class Solution {
 2public:
 3    int firstMissingPositive(vector<int>& nums) {
-4        int n = nums.size();
-5        int i = 0;
-6        while(i<n){
-7            if (nums[i]>0 && nums[i]<=n && nums[i]!=nums[nums[i]-1]) swap(nums[i], nums[nums[i]-1]);
-8            else i++;
-9        }
-10        for (int i=0 ; i<n ; i++){
-11            if (nums[i]!=i+1) return i+1;
-12        }
-13        return n+1;
-14    }
-15};
+4        unordered_set<int> st;
+5
+6        int maxi=0;
+7
+8        for (int i: nums){
+9            st.insert(i);
+10            maxi=max(i, maxi);
+11        }
+12
+13        for (int i=1;i<=maxi;i++){
+14            if (st.find(i)!=st.end()) continue;
+15            else return i;
+16        }
+17        return maxi+1;
+18    }
+19};
