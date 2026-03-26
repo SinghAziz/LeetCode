5f0b01abb5e012bd9768aab4fa912e882b2548a8
Har Aziz Singh <76054014+SinghAziz@users.noreply.github.com>
2026-03-27 01:39:56 +0530

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.cpp
@@ -0,0 +1,17 @@
+// Last updated: 27/03/2026, 01:39:56
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for (int i = digits.size()-1 ;i>=0 ;i--){
+            if (digits[i]==9){
+                digits[i]=0;
+            }
+            else{
+                digits[i]++;
+                return digits;
+            }
+        }
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
